31d78e72f7bcd23ba854dc6a53ac0e5995780e2e
Ivanka_Da <sepilovivan89@gmail.com>
2026-05-10 13:20:26 +0700

    ещё попытки

---
+++ Skrept.js
@@ -0,0 +1,13 @@
+var Apple = 49
+var Ball = 84
+var Cat = 144
+var Door = 378
+
+Apple = Apple + Ball + (Cat * Door)
+
+if(Apple > Ball){
+	console.log("> 50000")
+}else{
+	console.log("< 50000")
+}
+console.log("Yslovie sverhy, polych number cnizy"),console.log(Apple)
