db5eae736c51d806cd67a68cc940ad4f3177234a
Ivanka_Da <sepilovivan89@gmail.com>
2026-05-10 14:24:01 +0700

    Prikol

--- Skrept.js
+++ Skrept.js
@@ -1,22 +1,25 @@
-var age = 19
-var item = false
-var krytost = true
-var mama = true
+var itog = 0
+var shtyka = 0
+var esheshtyka = 0
+var kaka = 0
+var pisa = 0
 
-if(age > 18){
-	console.log("Bzrosluy")
-}else{
-	console.log("Maloy")
-}if(item = true){
-	console.log("Bec imeesh")
-}else{
-	console.log("Beca Hety")
-}if(krytost = true){
-	console.log("Tu kryt")
-}else{
-	console.log("Tu lox")
-}if(mama = true){
-	console.log("Mama imeetsy")
-}else{
-	console.log("Mamu nety")
-}
+itog = 2 + 10 * 5
+
+shtyka = shtyka + itog + 176
+
+esheshtyka = esheshtyka + itog + shtyka + 17
+
+kaka = kaka + 6 * 7
+
+pisa = pisa + 67 / 10
+
+kaka = kaka / pisa
+
+esheshtyka = esheshtyka * kaka + 0.998955223881 - 0.0000000000002
+
+itog = esheshtyka + itog
+
+itog = itog - 426.79
+
+console.log(itog)
